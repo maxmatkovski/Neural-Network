477da63bf739dd8cb04125a22ae7d00c0463a4a5
Max <74901101+datadrivenmax@users.noreply.github.com>
2024-05-07 21:42:52 -0700

    pass forward calculation function

--- Neuron.py
+++ Neuron.py
@@ -1,9 +1,29 @@
 # coding neural net from scratch
-inputs = [1.2, 5.1, 2.1] # input features
-weights = [3.1,2.1,8.7] # multiplicative relationship with the input value
-bias = 3
+inputs = [1,2,3,2.5] # input features
 
-# simple pass forward calculation
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+ # multiplicative relationship with the input value
+weights1 = [.2,.8,-.5,1.0]
+weights2 = [.5,-.91,.26,-.5]
+weights3 = [-.26, -.27,.17,.87]
 
-print(output)
+bias1 = 2
+bias2 = 3
+bias3 = .5
+
+# simple pass forward calculation function
+def pass_forward(inputs,weights,bias):
+    output = 0
+    for i in range(len(inputs)):
+        output += (inputs[i]*weights[i])
+    output += bias
+    return output
+
+result1 = pass_forward(inputs,weights1,bias1)
+result2 = pass_forward(inputs,weights2,bias2)
+result3 = pass_forward(inputs,weights3,bias3)
+
+
+final_result = [result1,result2,result3]
+print(final_result)
+
+
