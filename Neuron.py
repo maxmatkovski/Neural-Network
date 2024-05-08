f4ef4a0b9a0b6d8a6a94b605a9907b9836048691
Max <74901101+datadrivenmax@users.noreply.github.com>
2024-05-07 18:20:56 -0700

    neural net

--- Neuron.py
+++ Neuron.py
@@ -1,7 +1,9 @@
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1,2.1,8.7]
+# coding neural net from scratch
+inputs = [1.2, 5.1, 2.1] # input features
+weights = [3.1,2.1,8.7] # multiplicative relationship with the input value
 bias = 3
 
+# simple pass forward calculation
 output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
 
 print(output)
