46cac056b6e4c849ec355c62aeda406321b7d2c9
Max <74901101+datadrivenmax@users.noreply.github.com>
2024-05-11 17:52:15 -0700

    inputs and weights multiplication

--- Neuron.py
+++ Neuron.py
@@ -1,29 +1,12 @@
-# coding neural net from scratch
-inputs = [1,2,3,2.5] # input features
-
- # multiplicative relationship with the input value
-weights1 = [.2,.8,-.5,1.0]
-weights2 = [.5,-.91,.26,-.5]
-weights3 = [-.26, -.27,.17,.87]
-
-bias1 = 2
-bias2 = 3
-bias3 = .5
-
-# simple pass forward calculation function
-def pass_forward(inputs,weights,bias):
-    output = 0
-    for i in range(len(inputs)):
-        output += (inputs[i]*weights[i])
-    output += bias
-    return output
-
-result1 = pass_forward(inputs,weights1,bias1)
-result2 = pass_forward(inputs,weights2,bias2)
-result3 = pass_forward(inputs,weights3,bias3)
+import numpy as np 
 
 
-final_result = [result1,result2,result3]
-print(final_result)
+inputs = [1,2,3,2.5]
+weights =  [[0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87]]
 
+biases = [2, 3, 0.5]
 
+output = np.dot(weights, inputs) + biases
+print(output)
