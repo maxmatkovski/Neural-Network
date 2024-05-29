cf8684d744165661aa04e33f541892ecb8fa84c2
Max <74901101+datadrivenmax@users.noreply.github.com>
2024-05-28 18:22:33 -0700

    add

--- Current.py
+++ Current.py
@@ -1,5 +1,7 @@
 import math
 softmax_output = [.7,.1,.2]
+
+# one hot encoding just tells us what correct target variable is
 target_output = [1,0,0]
 
 loss = -(math.log(softmax_output[0]) * target_output[0] +
@@ -7,3 +9,4 @@ loss = -(math.log(softmax_output[0]) * target_output[0] +
          math.log(softmax_output[2]) * target_output[2])
 
 print(loss)
+print(-math.log(.7))
